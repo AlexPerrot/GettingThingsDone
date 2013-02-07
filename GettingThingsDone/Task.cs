cbea4f4665b0f98f93e35cf50ef67da780452935
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-07 20:41:15 +0100

    modif attributs task et ajout d'une interface Task pour démarrer doucement ^^

---
+++ GettingThingsDone/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GettingThingsDone
+{
+    interface Task
+    {
+        String Title { get; set; }
+        String Description { get; set; }
+        Boolean Done { get; set; }
+        DateTime DueDate { get; set; }
+        DateTime CreationDate { get; }
+    }
+}
