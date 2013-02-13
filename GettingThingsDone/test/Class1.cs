86cb56b52fcd204cb9578047601b0f4cf02d0e8a
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-13 15:03:54 +0100

    première classe de test

---
+++ GettingThingsDone/test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GettingThingsDone.test
+{
+    [TestClass]
+    class SingleTaskTest
+    {
+        [TestMethod]
+        public void testCreation()
+        {
+            Task st = new SingleTask();
+        }
+    }
+}
