7b7cc218512aa22b60dd617e0d39b64a0b2081f3
Clément BADIOLA <cidhair@free.fr>
2013-02-19 15:21:30 +0100

    Création du périmètre de synchronisation côté serveur. Suppression de tout périmètre déjà existant au préalable, car on ne peut pas mettre à jour le périmètre avec de nouveaux éléments (obliger de tout recréer).

--- ProvisionServer/Program.cs
+++ ProvisionServer/Program.cs
@@ -13,16 +13,40 @@ namespace ProvisionServer
     {
         static void Main(string[] args)
         {
-            SqlConnection serverConn = new SqlConnection(@"Data Source='.\SQLEXPRESS';Initial Catalog=gtd;Integrated Security=True");
+            // Defines the sync scope name
+            const string scopeName = "UserScope";
+
+            // Creates a connexion to the server database.
+            string SqlConnString = @"Data Source='.\SQLEXPRESS';Initial Catalog=gtd;Integrated Security=True";
+            SqlConnection serverConn = new SqlConnection(SqlConnString);
+
+            // Remove the previous scope from the Sql Server database (no support for updating a scope).
+            SqlSyncScopeDeprovisioning sqlDepro = new SqlSyncScopeDeprovisioning(serverConn);
+            sqlDepro.DeprovisionScope(scopeName);
 
             // Defines a new synchronization scope named UserScope
-            DbSyncScopeDescription scopeDesc = new DbSyncScopeDescription("UserScope");
+            DbSyncScopeDescription scopeDesc = new DbSyncScopeDescription(scopeName);
 
             // Gets the description of the Users table from the gtd database
             DbSyncTableDescription tableDesc = SqlSyncDescriptionBuilder.GetDescriptionForTable("Users", serverConn);
 
             // Adds the table description to the sync scope definition
             scopeDesc.Tables.Add(tableDesc);
+
+            // Gets the description of the other tables from the gtd base and add them to the sync scope.
+            DbSyncTableDescriptionCollection otherTables = new DbSyncTableDescriptionCollection();
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Tasks", serverConn));
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Lists", serverConn));
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Projects", serverConn));
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Tasks_Lists", serverConn));
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Tasks_Tasks", serverConn));
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Lists_Tasks", serverConn));
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Projects_Tasks", serverConn));
+            otherTables.Add(SqlSyncDescriptionBuilder.GetDescriptionForTable("Projects_Lists", serverConn));
+            foreach (var table in otherTables)
+            {
+                scopeDesc.Tables.Add(table);
+            }
 
             // Creates a server scope provisioning object based on the ProductScope
             SqlSyncScopeProvisioning serverProvision = new SqlSyncScopeProvisioning(serverConn, scopeDesc);
