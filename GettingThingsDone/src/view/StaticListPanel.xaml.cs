53363a5cf3e7630cdb13633e9375ffea9277f93a
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-20 10:56:14 +0100

    ajout fenetre d'édition au double click

--- GettingThingsDone/src/view/StaticListPanel.xaml.cs
+++ GettingThingsDone/src/view/StaticListPanel.xaml.cs
@@ -29,5 +29,17 @@ namespace GettingThingsDone.src.view
         {
             System.Console.WriteLine("Mouse on : " + DataContext);
         }
+
+        private void StackPanel_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                TaskCreationWindow editWin = new TaskCreationWindow();
+                SingleTask task = (sender as StackPanel).DataContext as SingleTask;
+                editWin.CreationPanel.LoadFromTask(task);
+                if (editWin.ShowDialog().Value)
+                    editWin.CreationPanel.WriteToTask(task);
+            }
+        }
     }
 }
