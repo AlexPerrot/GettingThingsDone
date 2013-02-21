bc5ed180983debeb0cf0166b345998bf7f3902bb
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-21 14:34:39 +0100

    ajout d'un usitlisateur admin (veillez à l'avoir en base)

--- GettingThingsDone/App.xaml.cs
+++ GettingThingsDone/App.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using System.Windows;
+using GettingThingsDone.Properties;
+using GettingThingsDone;
 
 namespace GettingThingsDone
 {
@@ -17,5 +20,17 @@ namespace GettingThingsDone
         private GTDSystem gtd = new GTDSystem();
         public GTDSystem GTD { get { return gtd; } }
 
+        private GettingThingsDone.DataClassesDataContext db = new DataClassesDataContext(Settings.Default.gtdLocalConnectionString);
+        public GettingThingsDone.DataClassesDataContext DB { get { return db; } }
+
+        private Users admin;
+        public Users Admin { get { return admin; } }
+
+
+        public App()
+            : base()
+        {
+            admin = db.Users.Single(item => item.Username == "admin");
+        }
     }
 }
