44034b610ef0e109a3e2dab2ccda78e9cfa7ce4b
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-23 12:28:49 +0100

    factory pour les objets en base

---
+++ GettingThingsDone/src/model/DBGTDFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.src.model
+{
+    class DBGTDFactory : GTDFactory
+    {
+
+        public ISingleTask makeTask(string title, string description, DateTimeOffset? DueDate)
+        {
+            DataClassesDataContext dc = new DataClassesDataContext();
+
+            Tasks dbTask = new Tasks();
+            dbTask.CreationDate = DateTimeOffset.Now;
+            dbTask.Title = title;
+            dbTask.Description = description;
+            dbTask.Owner = (App.Current as App).Admin.Id;
+
+            dc.Tasks.InsertOnSubmit(dbTask);
+            dc.SubmitChanges();
+
+            return new DBSingleTask(dbTask);
+        }
+
+        public IProject makeProject(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
