fcaf700f199a930d80cb5c7650f787bbf2c15c26
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-24 11:39:25 +0100

    suppression de la tache en base possible grace à l'interface graphique

--- GettingThingsDone/src/view/StaticListPanel.xaml.cs
+++ GettingThingsDone/src/view/StaticListPanel.xaml.cs
@@ -43,9 +43,10 @@ namespace GettingThingsDone.src.view
 
         private void DelButton_Click_1(object sender, RoutedEventArgs e)
         {
-            SingleTask task = (sender as Button).DataContext as SingleTask;
+            ISingleTask task = (sender as Button).DataContext as ISingleTask;
             StaticList list = DataContext as StaticList;
-            list.List.Remove(task);
+            list.removeTask(task);
+            task.Delete();
         }
     }
 }
