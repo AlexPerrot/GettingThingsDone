eb46bd15bc173557d0fe876e94063d6608a175bd
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-25 16:21:16 +0100

    un visiteur qui retourne un énumérateur qui donne toutes les taches (indifférament ISingleTask et IProject)

---
+++ GettingThingsDone/src/model/visitor/AllTasks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.src.model.visitor
+{
+    public class AllTasks : GTDVisitor<IEnumerable<Task>>
+    {
+
+        public IEnumerable<Task> visit(ISingleTask t)
+        {
+            yield return t;
+        }
+
+        public IEnumerable<Task> visit(IProject p)
+        {
+            yield return p;
+        }
+
+        public IEnumerable<Task> visit(IGTDSystem s)
+        {
+            IEnumerable<Task> tmp = new List<Task>();
+            foreach (GTDItem i in s)
+                tmp = tmp.Concat(i.accept(this));
+            return tmp;
+        }
+
+        public IEnumerable<Task> visit(IStaticList l)
+        {
+            IEnumerable<Task> tmp = new List<Task>();
+            foreach (GTDItem i in l)
+                tmp = tmp.Concat(i.accept(this));
+            return tmp;
+        }
+
+        public IEnumerable<Task> visit(IDynamicList l)
+        {
+            IEnumerable<Task> tmp = new List<Task>();
+            foreach (GTDItem i in l)
+                tmp = tmp.Concat(i.accept(this));
+            return tmp;
+        }
+    }
+}
