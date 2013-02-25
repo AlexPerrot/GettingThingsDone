0c1b2848df784109a28831a2248b10d3f99cdeff
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-25 15:31:14 +0100

    ajout de propriétés pour le IGTDSystem

--- GettingThingsDone/src/model/IGTDSystem.cs
+++ GettingThingsDone/src/model/IGTDSystem.cs
@@ -8,5 +8,12 @@ namespace GettingThingsDone.src.model
 {
     public interface IGTDSystem : TaskList
     {
+        TaskList Inbox { get; }
+        TaskList Today { get; }
+        TaskList Tommorow { get; }
+        TaskList Someday { get; }
+        TaskList Waiting { get; }
+        IEnumerable<TaskList> contexts { get; }
+
     }
 }
