c656ad08872765eeeb47fcb4385cb200237d75eb
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-25 17:26:24 +0100

    visiteur pour récupérer les sous-listes (pas fini)

---
+++ GettingThingsDone/src/model/visitor/SubLists.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.src.model.visitor
+{
+    public class SubLists : GTDVisitor<IEnumerable<TaskList>>
+    {
+
+        public IEnumerable<TaskList> visit(ISingleTask t)
+        {
+            return Enumerable.Empty<TaskList>();
+        }
+
+        public IEnumerable<TaskList> visit(IProject p)
+        {
+            return Enumerable.Empty<TaskList>();
+        }
+
+        public IEnumerable<TaskList> visit(IGTDSystem s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TaskList> visit(IStaticList l)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TaskList> visit(IDynamicList l)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
