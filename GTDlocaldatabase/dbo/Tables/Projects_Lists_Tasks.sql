4873922eb41bf4fbb25dc934a497d1218d1882e1
Clément BADIOLA <cidhair@free.fr>
2013-02-25 17:58:42 +0100

    Ajout de la nouvelle table. Calqué sur les modification du projet de déploiement bdd côté serveur.

---
+++ GTDlocaldatabase/dbo/Tables/Projects_Lists_Tasks.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Projects_Lists_Tasks] (
+    [Id]              INT IDENTITY (1, 1) NOT NULL,
+    [Project_List_id] INT NOT NULL,
+    [Owner]           INT NOT NULL,
+    [Task_id]         INT NOT NULL,
+    CONSTRAINT [PK_Projects_Lists_Tasks] PRIMARY KEY CLUSTERED ([Id] ASC, [Owner] ASC),
+    CONSTRAINT [FK_Projects_Lists_Tasks_Projects_Lists] FOREIGN KEY ([Project_List_id], [Owner]) REFERENCES [dbo].[Projects_Lists] ([Id], [Owner]),
+    CONSTRAINT [FK_Projects_Lists_Tasks_Tasks] FOREIGN KEY ([Task_id], [Owner]) REFERENCES [dbo].[Tasks] ([Id], [Owner]),
+    CONSTRAINT [FK_Projects_Lists_Tasks_Users] FOREIGN KEY ([Owner]) REFERENCES [dbo].[Users] ([Id])
+);
+
