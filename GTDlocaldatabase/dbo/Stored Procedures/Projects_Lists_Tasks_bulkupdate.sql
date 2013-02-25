2b4b71a531eec50855b94a5e78a5797973108a98
Clément BADIOLA <cidhair@free.fr>
2013-02-25 18:05:16 +0100

    Correction temporaire du périmètre de synchronisation de la bdd locale. Basé sur celui du serveur (d'où le temporaire). Evite la génération d'erreurs SQL pénibles.

--- GTDlocaldatabase/dbo/Stored Procedures/Projects_Lists_Tasks_bulkupdate.sql
+++ GTDlocaldatabase/dbo/Stored Procedures/Projects_Lists_Tasks_bulkupdate.sql
@@ -1,29 +1,29 @@
-﻿CREATE PROCEDURE [Projects_Lists_bulkupdate]
+﻿CREATE PROCEDURE [Projects_Lists_Tasks_bulkupdate]
 	@sync_min_timestamp BigInt,
 	@sync_scope_local_id Int,
-	@changeTable [Projects_Lists_BulkType] READONLY
+	@changeTable [Projects_Lists_Tasks_BulkType] READONLY
 AS
 BEGIN
 -- use a temp table to store the list of PKs that successfully got updated
 declare @changed TABLE ([Id] int, [Owner] int, PRIMARY KEY ([Id], [Owner]));
 
-SET IDENTITY_INSERT [Projects_Lists] ON;
+SET IDENTITY_INSERT [Projects_Lists_Tasks] ON;
 -- update the base table
-MERGE [Projects_Lists] AS base USING
+MERGE [Projects_Lists_Tasks] AS base USING
 -- join done here against the side table to get the local timestamp for concurrency check
-(SELECT p.*, t.update_scope_local_id, t.scope_update_peer_key, t.local_update_peer_timestamp FROM @changeTable p LEFT JOIN [Projects_Lists_tracking] t ON p.[Id] = t.[Id] AND p.[Owner] = t.[Owner]) as changes ON changes.[Id] = base.[Id] AND changes.[Owner] = base.[Owner]
+(SELECT p.*, t.update_scope_local_id, t.scope_update_peer_key, t.local_update_peer_timestamp FROM @changeTable p LEFT JOIN [Projects_Lists_Tasks_tracking] t ON p.[Id] = t.[Id] AND p.[Owner] = t.[Owner]) as changes ON changes.[Id] = base.[Id] AND changes.[Owner] = base.[Owner]
 WHEN MATCHED AND (changes.update_scope_local_id = @sync_scope_local_id AND changes.scope_update_peer_key = changes.sync_update_peer_key) OR changes.local_update_peer_timestamp <= @sync_min_timestamp THEN
-UPDATE SET [Project_id] = changes.[Project_id], [Title] = changes.[Title], [Description] = changes.[Description]
+UPDATE SET [Project_List_id] = changes.[Project_List_id], [Task_id] = changes.[Task_id]
 OUTPUT INSERTED.[Id], INSERTED.[Owner] into @changed; -- populates the temp table with successful PKs
 
-SET IDENTITY_INSERT [Projects_Lists] OFF;
+SET IDENTITY_INSERT [Projects_Lists_Tasks] OFF;
 UPDATE side SET
 update_scope_local_id = @sync_scope_local_id, 
 scope_update_peer_key = changes.sync_update_peer_key, 
 scope_update_peer_timestamp = changes.sync_update_peer_timestamp,
 local_update_peer_key = 0
 FROM 
-[Projects_Lists_tracking] side JOIN 
+[Projects_Lists_Tasks_tracking] side JOIN 
 (SELECT p.[Id], p.[Owner], p.sync_update_peer_timestamp, p.sync_update_peer_key, p.sync_create_peer_key, p.sync_create_peer_timestamp FROM @changed t JOIN @changeTable p ON p.[Id] = t.[Id] AND p.[Owner] = t.[Owner]) as changes ON changes.[Id] = side.[Id] AND changes.[Owner] = side.[Owner]
 SELECT [Id], [Owner] FROM @changeTable t WHERE NOT EXISTS (SELECT [Id], [Owner] from @changed i WHERE t.[Id] = i.[Id] AND t.[Owner] = i.[Owner])
 END
--- GTDlocaldatabase/dbo/User Defined Types/Projects_Lists_BulkType.sql
+++ GTDlocaldatabase/dbo/User Defined Types/Projects_Lists_BulkType.sql
@@ -1,13 +1,16 @@
 ﻿CREATE TYPE [dbo].[Projects_Lists_BulkType] AS TABLE (
-    [Id]                         INT    NOT NULL,
-    [Project_id]                 INT    NULL,
-    [List_id]                    INT    NULL,
-    [Owner]                      INT    NOT NULL,
-    [sync_update_peer_timestamp] BIGINT NULL,
-    [sync_update_peer_key]       INT    NULL,
-    [sync_create_peer_timestamp] BIGINT NULL,
-    [sync_create_peer_key]       INT    NULL,
+    [Id]                         INT            NOT NULL,
+    [Project_id]                 INT            NULL,
+    [Owner]                      INT            NOT NULL,
+    [Title]                      NVARCHAR (50)  NULL,
+    [Description]                NVARCHAR (MAX) NULL,
+    [sync_update_peer_timestamp] BIGINT         NULL,
+    [sync_update_peer_key]       INT            NULL,
+    [sync_create_peer_timestamp] BIGINT         NULL,
+    [sync_create_peer_key]       INT            NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC, [Owner] ASC));
+
+
 
 
 
