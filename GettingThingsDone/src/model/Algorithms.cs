9685827389f6e060c9ab18ac849069a3fcf95cbc
sjolliet <simon.jolliet@etu.u-bordeaux1.fr>
2013-02-27 23:36:12 +0100

    Ajout classe d'algos pour filtre liste dynamique

---
+++ GettingThingsDone/src/model/Algorithms.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.src.model
+{
+    class Algorithms
+    {
+        public static bool getDueToday(GTDItem item)
+        {
+            Task t = (Task)item;
+            if (t.DueDate == DateTime.Today)
+                return true;
+            return false;
+        }
+    }
+}
