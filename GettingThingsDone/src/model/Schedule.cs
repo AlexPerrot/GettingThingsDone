55653592c79f0fe57039fcad2e9f6d86a7ec6561
AlexPerrot <alexandre.perrot@gmail.com>
2013-02-28 13:42:41 +0100

    ajout d'une classe Schedule pour modéliser l'échéancier

---
+++ GettingThingsDone/src/model/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.ComponentModel;
+
+using GettingThingsDone.src.model.visitor;
+
+namespace GettingThingsDone.src.model
+{
+    public class Schedule
+    {
+        private IGTDSystem sys;
+
+        public Schedule(IGTDSystem system)
+        {
+            sys = system;
+        }
+
+        public IEnumerable Today { get { 
+            IEnumerable tasks = sys.accept(new AllTasks());
+            ICollectionView view = CollectionViewSource.GetDefaultView(tasks);
+            view.Filter = isDueToday;
+            return view;
+        } }
+
+        private bool isDueToday(Object obj)
+        {
+            return (obj as Task).DueDate.Value.Day.Equals(DateTime.Now.Day);
+        }
+    }
+}
