d6159e20a75d344e6751faea356c61d7313d30a0
AlexPerrot <alexandre.perrot@gmail.com>
2013-03-11 11:37:37 +0100

    ajout de la classe de selection des template

---
+++ GettingThingsDone/src/view/TaskDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using GettingThingsDone.src.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GettingThingsDone.src.view
+{
+    class TaskDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SingleTaskTemplate { get; set; }
+        public DataTemplate ProjectTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is IProject)
+                return ProjectTemplate;
+            else if (item is ISingleTask)
+                return SingleTaskTemplate;
+            else
+                return base.SelectTemplate(item, container);
+        }
+    }
+}
