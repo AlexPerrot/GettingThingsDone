2d814cbda07602ec4228535138bb697fe9c3b69b
AlexPerrot <alexandre.perrot@gmail.com>
2013-03-11 13:59:35 +0100

    ajout de label pour ajouter une tache

--- GettingThingsDone/src/view/ProjectsPanel.xaml.cs
+++ GettingThingsDone/src/view/ProjectsPanel.xaml.cs
@@ -70,5 +70,11 @@ namespace GettingThingsDone.src.view
             }
         }
 
+        private void Label_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            Task t = TaskCreationWindowWithContext.NewTaskDialog();
+            (FileList.SelectedItem as IProject).Tasks.Add(t);
+        }
+
     }
 }
