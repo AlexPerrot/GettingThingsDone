be9d9c5c757324d650d23a2694f242d807b61c0a
mickael-dales <dada232@hotmail.fr>
2013-03-15 16:12:58 +0100

    Ajout de l'interface user et de la classe dbuser

---
+++ GettingThingsDone/src/model/DBUser.cs
@@ -0,0 +1,82 @@
+﻿using GettingThingsDone.src.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.src.data
+{
+    class DBUser : IUser
+    {
+        String Username 
+        {
+            get
+            {
+                DataClassesDataContext db = dbProvider.Database;
+                Users u = db.Users.Single(x => x.Id == id);
+                return u.Username; 
+            }
+            
+            set
+            {
+                DataClassesDataContext db = dbProvider.Database;
+                Users u = db.Users.Single(x => x.Id == id);
+                u.Username = value;
+                db.SubmitChanges(); 
+            } 
+        }
+        String Password 
+        {
+            get
+            {
+                DataClassesDataContext db = dbProvider.Database;
+                Users u = db.Users.Single(x => x.Id == id);
+                return u.Password; 
+            }
+            
+            set
+            {
+                DataClassesDataContext db = dbProvider.Database;
+                Users u = db.Users.Single(x => x.Id == id);
+                u.Password = value;
+                db.SubmitChanges(); 
+            } 
+        }
+        String Mail { 
+            get
+            {
+                DataClassesDataContext db = dbProvider.Database;
+                Users u = db.Users.Single(x => x.Id == id);
+                return u.Mail; 
+            }
+            
+            set
+            {
+                DataClassesDataContext db = dbProvider.Database;
+                Users u = db.Users.Single(x => x.Id == id);
+                u.Mail = value;
+                db.SubmitChanges(); 
+            } 
+        }
+
+        public void Delete()
+        {
+            DataClassesDataContext db = dbProvider.Database;
+            Users u = db.Users.Single(x => x.Id == this.id);
+            db.Users.DeleteOnSubmit(u);
+            db.SubmitChanges();
+        }
+
+        private int id;
+
+        private IDatabaseProvider dbProvider;
+
+        public DBUser(Users user, IDatabaseProvider dbProvider)
+        {
+            this.id = user.Id;
+            this.dbProvider = dbProvider;
+        }
+    }
+    }
+}
---
+++ GettingThingsDone/src/model/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingThingsDone.src.model
+{
+    public interface IUser
+    {
+        String Username { get; set; }
+        String Password { get; set; }
+        String Mail { get; set; }
+    }
+}
